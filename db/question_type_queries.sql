427bd2064ee93bc7a79a00c4c5fb48c39b60d8a1
Martin Kubis <mkubis@ksu.edu>
2016-12-06 14:03:38 -0600

    Source file for question type queries

---
+++ db/question_type_queries.sql
@@ -0,0 +1,19 @@
+-- 1) Which team has the most of gyms? Tyler
+
+-- 2) Which pokemons were caught the most times? Erisa
+
+-- 3) Range of price of items? Tyler
+
+-- 4) Which trainers have the most pokemons? Erisa
+
+-- 5) Which pokemons haven't been caught? Martin
+
+-- 6) What are the average healing points among all healing items? Yisi
+
+-- 7) Which members are leading the most gyms (including gym names)? Yisi
+
+-- 8) Who owns the most valuable item among all kinds? Yisi
+
+-- 9) In which cities can be healing items found? Martin
+
+-- 10) Which trainer catch the most pokemons per day? Erisa
