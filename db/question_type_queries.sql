770b4dfe1a0fafa0b6094af460da628463136be4
Martin Kubis <mkubis@ksu.edu>
2016-12-06 14:47:14 -0600

    First question type queries

--- db/question_type_queries.sql
+++ db/question_type_queries.sql
@@ -7,14 +7,23 @@
 -- 4) Which trainers have the most pokemons? Erisa
 
 -- 5) Which pokemons haven't been caught? Martin
-
+select *
+from pokemons
+where pokemon_name not in (
+    select p.pokemon_name
+    from pokemons p, pokemons_trainers pt
+    where p.pokemon_name = pt.pokemon_name
+);
 -- 6) What are the average healing points among all healing items? Yisi
 
 -- 7) Which members are leading the most gyms (including gym names)? Yisi
 
 -- 8) Who owns the most valuable item among all kinds? Yisi
 
--- 9) In which cities can be healing items found? Martin
-
+-- 9) In which cities can be combat items found? Martin
+select ci.item_name, group_concat(distinct city_name,", ", loc separator "; ") cities
+from stores_items si, combat_items ci
+where ci.item_name = si.item_name
+group by ci.item_name;
 -- 10) Which trainer catch the most pokemons per day? Erisa
 --
