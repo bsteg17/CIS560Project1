c803aaacd635a328c09e39b0efcba94188684abf
bsteg17 <bsteg17@gmail.com>
2016-12-10 18:15:28 -0600

    fixed pokemon show page

--- front_end/db/migrate/20161206192509_create_gyms.rb
+++
@@ -1,9 +0,0 @@
-class CreateGyms < ActiveRecord::Migration[5.0]
-  def change
-    create_table :gyms do |t|
-      t.integer :id
-
-      t.timestamps
-    end
-  end
-end
--- front_end/db/migrate/20161209054911_create_stores.rb
+++
@@ -1,9 +0,0 @@
-class CreateStores < ActiveRecord::Migration[5.0]
-  def change
-    create_table :stores do |t|
-      t.string :location
-
-      t.timestamps
-    end
-  end
-end
